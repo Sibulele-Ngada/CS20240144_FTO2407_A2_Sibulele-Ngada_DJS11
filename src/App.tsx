89b6855243bbe7b2aec80de79f6b326e69bf7fb5
Sibulele Ngada <sibulelengada@gmail.com>
2024-11-25 15:42:29 +0200

    Display episode data

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path=":show" element={<Show />} />
+          <Route path=":id" element={<Show />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Show.tsx
+++ src/pages/Show.tsx
@@ -1,3 +1,64 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+
+type Show = {
+  description: string;
+  genres: number[];
+  id: string;
+  image: string;
+  seasons: Season[];
+  title: string;
+  updated: string;
+};
+
+type Season = {
+  episodes: Episode[];
+  image: string;
+  season: number;
+  title: string;
+};
+
+type Episode = {
+  description: string;
+  episode: number;
+  file: string;
+  title: string;
+};
+
 export default function Show() {
-  return <h1>Show Page</h1>;
+  const [currentShow, setCurrentShow] = useState<Show>();
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`https://podcast-api.netlify.app/id/${id}`)
+      .then((res) => {
+        if (!res.ok) throw new Error();
+        return res.json();
+      })
+      .then((data) => setCurrentShow(data))
+      .catch(() => console.log(`Error fetching show`));
+  }, [id]);
+
+  const title = currentShow ? currentShow.title : "no data";
+  const seasons: Season[] = currentShow ? currentShow.seasons : [];
+  const seasonElements = seasons.map((season) => {
+    return (
+      <>
+        <h4>
+          Season {season.season}: {season.episodes.length} Episodes
+        </h4>
+        <a href={season.episodes[0].file} target="_blank">
+          {season.episodes[0].title}
+        </a>
+      </>
+    );
+  });
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <h2>Seasons: {seasons?.length}</h2>
+      <div>{seasonElements}</div>
+    </>
+  );
 }
